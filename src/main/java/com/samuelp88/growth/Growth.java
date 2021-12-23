d18c6ada673e69a77ed184e2ba47f4ac64d610db
Focamacho <renancolombomail@gmail.com>
2021-12-23 03:20:52 -0300

    Remove unused code and comments

--- src/main/java/com/samuelp88/growth/Growth.java
+++ src/main/java/com/samuelp88/growth/Growth.java
@@ -32,11 +32,9 @@ import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("growth")
 public class Growth {
 
-    // Directly reference a log4j logger.
     public static final String MODID = "growth";
     public static final CreativeModeTab CREATIVETAB = new CreativeModeTab(MODID) {
         @Override
@@ -44,26 +42,13 @@ public class Growth {
             return new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation("growth:growth_potion")));
         }
     };
-    public static final Logger LOGGER = LogManager.getLogger();
 
     public Growth() {
-        // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doLaterStuff);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
         RegistryHandler.registerBrewings();
     }
 
@@ -118,28 +103,8 @@ public class Growth {
         });
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-    }
-
-    private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class RegistryEvents {
+    public static class ClientRegistryEvents {
 
         @SubscribeEvent
         public static void onEntityRendererRegistry(final EntityRenderersEvent.RegisterRenderers event) {
